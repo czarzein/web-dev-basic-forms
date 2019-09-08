646159f76e95e038bf4b6229fb7f1108bb9316c7
czarzein <czar.zeinovic@gmail.com>
2019-09-08 15:39:09 +0800

    Add getInput function

---
+++ main.js
@@ -0,0 +1,5 @@
+function getInput() {
+    let input = document.getElementById('textInput').value;
+    let output = document.getElementById('textOutput');
+    output.setAttribute('value',input);
+}
